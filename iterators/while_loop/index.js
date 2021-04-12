9b3977fda33775e436ed3b2524f4d03b32830d92
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-12 11:52:47 +0200

    Add files via upload

---
+++ iterators/while_loop/index.js
@@ -0,0 +1,19 @@
+// const colors = (["yellow, blue, red, orange"]);
+
+// while (colors < 10) {
+//     console.log("yess");
+//     colors++;
+// }
+// document.write("done")
+
+
+
+
+for (i = ["yellow, blue, red, orange"]; i < aray.lenght; i++) {
+    console.log("yepp" + i);
+}
+
+document.write("alles is gelooped");
+
+
+
