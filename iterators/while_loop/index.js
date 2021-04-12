e2619545d55f8d3678b49ce7b8b6c5e0cb8dd785
Newien Gopie <33331871+NSG-Company@users.noreply.github.com>
2021-04-12 12:09:11 +0200

    Add files via upload

---
+++ iterators/while_loop/index.js
@@ -0,0 +1,21 @@
+const colors = ["yellow", "blue", "red", "orange"];
+
+let i = 0;
+while (i < colors.lenght) {
+    console.log(colors[i]);
+    i++;
+}
+
+
+
+
+for (let i = 0; i < colors.lenght; i++) {
+    console.log(colors + [i]);
+}
+// nieuwe manier=
+myColorArray.forEach((color) => console.log(color));
+
+
+
+
+
